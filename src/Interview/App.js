d250cdfb1390a702c508eaab8c1e0086cc39b7f8
Youstina Hanna <60597108+UstinaHanna@users.noreply.github.com>
2023-12-04 14:21:27 +0200

    Add files via upload

---
+++ src/Interview/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SpeechReader from './SpeechReader';
+
+const App = () => {
+  return (
+    <div>
+      <SpeechReader />
+    </div>
+  );
+};
+
+export default App;
