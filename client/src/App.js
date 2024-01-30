75feecdf316122c3343e96eeccee448b1258da4e
maryamabdou <maryamabdou2000@gmail.com>
2024-01-30 23:32:57 +0200

    added interview page impl

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Interview from './pages/Interview/Interview';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+  return (
+    <div>
+      <Interview />
+    </div>
+  )
+}
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  
+  return(
+    <div>
+        <h5>Navbar</h5>
+    </div>
+  );
+};
+
+export default Navbar;
+
