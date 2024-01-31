569cffc01f9f4bbd09fa17d296b7546e4ba8076b
maryamabdou <maryamabdou2000@gmail.com>
2024-01-31 22:01:37 +0200

    solved shifted button

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Button.css';
 
-function Button({ label }) {
+function Button({ label, onClick }) {
+  const handleClick = () => {
+    // You can also perform other actions or state updates here
+    onClick(); // Invoke the provided onClick function
+  };
+
   return (
-    <button className="button">
+    <button className="button" onClick={handleClick}>
       {label}
     </button>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,13 +7,14 @@ function Navbar() {
   
   return(
     <div className = "NavBar">
-            <div className="logo-container">
+      <div className="logo-container">
         <img src={logo} alt="Logo" className="logo" />
-      </div>
-      <div className="content">
         <div className="title">
           <h1>APIA</h1>
         </div>
+      </div>
+      
+      <div className="content">
         <NavButton label="Stop" onClick={() => console.log('Button clicked!')} />
       </div>
     </div>
