0593f10dc6e57de3317958f009fb89eb09ec6bca
Farah Elgendy <elgendyfarah3@gmail.com>
2024-02-11 20:51:27 +0200

    editing the avatar

--- client/src/pages/Interview/Avatar.jsx
+++ client/src/pages/Interview/Avatar.jsx
@@ -1,18 +1,28 @@
+/*import React, { useEffect, useRef } from "react";*/
 import React from "react";
-import videoFile from './result_video.mp4'; // Import your video file
+import videoFile from './result_video.mp4';
+import './Avatar.css';
+import { Player , BigPlayButton} from 'video-react';
 
 
 function Avatar() {
+/*  const vidRef = useRef();
+
+  useEffect(() => {
+    vidRef.current.muted = false;
+  }, []);*/
+  
+
   return(
-    <div>
-      <center>
-        <video width="600" height="400" controls>
+    
+    <div className="avatar">
+      <Player autoPlay loop fluid={false} disableDefaultControls disableFullscreen>
         <source src={videoFile} type="video/mp4" />
-      </video>
-      </center>
+        <BigPlayButton/>
+      </Player>
+
     </div>
   );
 };
 
 export default Avatar;
-
