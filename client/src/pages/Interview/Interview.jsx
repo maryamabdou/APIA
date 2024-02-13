95abbc29dad492d283be1ebfcf1ad660b7205d50
ShamsZayan <89603134+ShamsZayan@users.noreply.github.com>
2024-02-13 17:25:18 +0200

    Add files via upload

--- client/src/pages/Interview/Interview.jsx
+++ client/src/pages/Interview/Interview.jsx
@@ -5,22 +5,30 @@ import WebGazer from './WebGazer';
 import SpeechReader from "./SpeechReader";
 import Avatar from "./Avatar.jsx";
 import SpeechToText from "./SpeechToText.jsx"
+import Sentsim from "./Sentsim.jsx"
 
 function Interview() {
   
   return(
     <div>
-      <WebGazer />
-      <Stack direction="column" spacing={2}>
+      <Stack direction="column" spacing={1}>
         <Box sx={{ height: "7vh"}}>
           <Navbar />
+        </Box>
+
+        <Box sx={{ height: "3vh"}}>
+          <WebGazer />
         </Box>
 
         <Box sx={{ height: "75vh"}}>
           <Avatar />
         </Box>
 
-        <Box sx={{ backgroundColor: "white", height: "7vh"}}>
+        <Box sx={{ height: "75vh"}}>
+          <Sentsim />
+        </Box>
+
+        <Box sx={{ backgroundColor: "white", height: "6vh"}}>
           <SpeechReader />
         </Box>
 
