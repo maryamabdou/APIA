59c05d0d781ba85793224faab96d459e4729fda5
ShamsZayan <89603134+ShamsZayan@users.noreply.github.com>
2024-02-13 16:43:00 +0200

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,31 @@ import Interview from './pages/Interview/Interview';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-
+  const [data, setData] = useState([{}])
+  useEffect (()=> {
+     fetch("/members").then(
+          res => res.json()
+     ).then (
+          data => {
+             setData(data)
+             console. log(data)
+          }
+     )
+   },[] )
   return (
     <div>
       <Interview />
-    </div>
+      {(typeof data.members === 'undefined') ?(
+       <p> loading... </p>
+    ): (
+       data.members.map((member, i) => (
+             <p key={i}>{member} </p>
+       ))
+    )}
+     </div>
+   
+     
+ 
   )
 }
 
