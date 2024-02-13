c3336f3128105ced6226f7a225152d47fd4ae42a
ShamsZayan <89603134+ShamsZayan@users.noreply.github.com>
2024-02-13 17:08:18 +0200

    Add files via upload

---
+++ client/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Interview from './pages/Interview/Interview';
+import React from 'react';
+
+function App() {
+
+  return (
+    <div>
+      <Interview />
+     </div>
+   
+     
+ 
+  )
+}
+
+export default App;
---
+++ client/Interview/Interview.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { Box, Stack } from "@mui/material";
+import Navbar from "../../components/Navbar.jsx";
+import WebGazer from './WebGazer';
+import SpeechReader from "./SpeechReader";
+import Avatar from "./Avatar.jsx";
+import SpeechToText from "./SpeechToText.jsx"
+import Sentsim from "./Sentsim.jsx"
+
+function Interview() {
+  
+  return(
+    <div>
+      <Stack direction="column" spacing={1}>
+        <Box sx={{ height: "7vh"}}>
+          <Navbar />
+        </Box>
+
+        <Box sx={{ height: "3vh"}}>
+          <WebGazer />
+        </Box>
+
+        <Box sx={{ height: "75vh"}}>
+          <Avatar />
+        </Box>
+
+        <Box sx={{ height: "75vh"}}>
+          <Sentsim />
+        </Box>
+
+        <Box sx={{ backgroundColor: "white", height: "6vh"}}>
+          <SpeechReader />
+        </Box>
+
+        <Box sx={{ backgroundColor: "white", height: "5vh"}}>
+          <SpeechToText />
+        </Box>
+      </Stack>
+    </div>
+  );
+};
+
+export default Interview;
+
