07289aad5a061263ab85d535c911c18381df54ac
ShamsZayan <89603134+ShamsZayan@users.noreply.github.com>
2024-02-13 16:40:44 +0200

    Add files via upload

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,17 @@
 from flask import Flask;
-
+from sentencesimilarity import  *
+from sentence_transformers import SentenceTransformer, util
+mode = '1'
 app = Flask(__name__)
-
+if mode == '1':
+    cosine_scores = sentSim(1)
+    @app.route("/members")
+    def members():
+        return {"members":[float(cosine_scores[0][0]),float(cosine_scores[1][1]),float(cosine_scores[2][2])]}
 @app.route("/")
 def home():
     return "hello"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
