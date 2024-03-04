69f15ac03e40b8d0d4302635b20b39d470e1d165
Youmna-Elmezayen <yuma_elmezain@gmx.com>
2024-03-04 15:52:51 +0200

    converted csv to json

--- client/src/scripts/Upload.jsx
+++ client/src/scripts/Upload.jsx
@@ -7,6 +7,10 @@ import { getDownloadURL, listAll, ref as storageRef } from "firebase/storage";
 import { ref as databaseRef, onValue } from "firebase/database";
 // import csvtojson from 'csvtojson';
 // import fs from "fs";
+import Papa from "papaparse";
+// import csv from "../assets/Software Questions.csv";
+// import csv from 'csv-parse';
+// import {Buffer} from 'buffer';
 
 const Upload = () => {
     // const [questions, setQuest] = useState("")
@@ -32,22 +36,48 @@ const Upload = () => {
     });
     };
 
-    const uploadQuestions = () => {
-        const csvtojson = require('csvtojson');
-        const fs = require('fs');
+    const uploadQuestions = (event) => {
+        // const csvtojson = require('csvtojson');
+        // const fs = require('fs');
 
-        const jsonFilePath = '../pages/Interview/output.json';
+        // const jsonFilePath = '../pages/Interview/output.json';
 
-        // Convert CSV to JSON
-        csvtojson()
-        .fromFile(csv_path)
-        .then((jsonArrayObj) => {
-            // Save the JSON data to a file
-            fs.writeFileSync(jsonFilePath, JSON.stringify(jsonArrayObj, null, 2));
-            console.log('Conversion successful. JSON file saved at', jsonFilePath);
-        })
-        .catch((error) => console.error('Error:', error));
+        // // Convert CSV to JSON
+        // csvtojson()
+        // .fromFile(csv_path)
+        // .then((jsonArrayObj) => {
+        //     // Save the JSON data to a file
+        //     // fs.writeFileSync(jsonFilePath, JSON.stringify(jsonArrayObj, null, 2));
+        //     console.log('Conversion successful. JSON file saved at', jsonArrayObj);
+        // })
+        // .catch((error) => console.error('Error:', error));
 
+        // console.log("Hello");
+        Papa.parse(event.target.files[0], {
+            complete: function(results) {
+                console.log("Finished:", results.data);
+            }
+        });
+        
+
+        // const csv = require('csv-parser')
+        // const fs = require('fs')
+        // const results = [];
+
+        // fs.createReadStream(csv_path)
+        // .pipe(csv())
+        // .on('data', (data) => results.push(data))
+        // .on('end', () => {
+        //     console.log(results);
+        //     // [
+        //     //   { NAME: 'Daffy Duck', AGE: '24' },
+        //     //   { NAME: 'Bugs Bunny', AGE: '22' }
+        //     // ]
+        // });
+
+
+
+        //IMPORTANT
         // for (let i = 0; i < totalQuestions; i++) {
         //     const question = questions.result[i].question;
         //     quest.push(question);
@@ -59,7 +89,14 @@ const Upload = () => {
 
     return (
     <div>
-        <button onClick={uploadQuestions}>Upload</button>
+    <input 
+        type="file"
+        name="file"
+        accept=".csv"
+        onChange={uploadQuestions}
+        >
+    </input>
+        {/* <button onClick={uploadQuestions}>Upload</button> */}
     </div>
     );
 };
