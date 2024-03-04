d0158e535953ee04b06d7d9ca4b6075cbf85f66c
Youstina Hanna <60597108+UstinaHanna@users.noreply.github.com>
2024-03-04 19:41:11 +0200

    added loginpopup and firstpage

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Interview from "./pages/Interview/Interview";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main/Main";
+import Firstpage from "./pages/FirstPage/Firstpage";
 
 function App() {
   return (
@@ -13,7 +14,9 @@ function App() {
       <Router>
         <Routes>
         <Route path="/" element={<Main />} />
-          <Route path="/login" element={<LoginPage />} />
+        
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+          <Route path="/firstpage" element={<Firstpage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/main" element={<MainPage />} />
         </Routes>
