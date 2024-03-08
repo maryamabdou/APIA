398f1c910d0a8129584d7d31eaf62e67c135c92f
maryamabdou <maryamabdou2000@gmail.com>
2024-03-08 23:47:10 +0200

    Update server.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -30,41 +30,41 @@ def upload_audio():
     print(received_text[0])
     print(fileName)
     index = 0
-    for question in received_text:
-        # tts = gTTS(question, lang='en-us')
-        # audio_path = 'Avatar/audio.wav'
-        # tts.save(audio_path)
-        engine = pyttsx3.init()
-        rate = engine.getProperty('rate')
-        engine.setProperty('rate', rate-10)
-        voices = engine.getProperty('voices')
-        engine.setProperty('voice', voices[10].id)
-        engine.save_to_file(question, "Avatar/audio.wav")
-        engine.runAndWait()
+    # for question in received_text:
+    #     # tts = gTTS(question, lang='en-us')
+    #     # audio_path = 'Avatar/audio.wav'
+    #     # tts.save(audio_path)
+    #     engine = pyttsx3.init()
+    #     rate = engine.getProperty('rate')
+    #     engine.setProperty('rate', rate-10)
+    #     voices = engine.getProperty('voices')
+    #     engine.setProperty('voice', voices[10].id)
+    #     engine.save_to_file(question, "Avatar/audio.wav")
+    #     engine.runAndWait()
 
-        # audio_name = "audio"+str(uuid.uuid4())+".wav"
-        # audio_data = {
-        #     "id": index,
-        #     "filename": audio_name
-        # }
-        # storage.child(fileName+"audios/"+audio_name).put("Avatar/audio.wav")
-        # database.child(fileName+"audios").child(str(index)).set(audio_data)
+    #     audio_name = "audio"+str(uuid.uuid4())+".wav"
+    #     audio_data = {
+    #         "id": index,
+    #         "filename": audio_name
+    #     }
+    #     storage.child(fileName+"audios/"+audio_name).put("Avatar/audio.wav")
+    #     database.child(fileName+"audios").child(str(index)).set(audio_data)
         
-        # command = "python3 Avatar/Wav2Lip/inference.py --checkpoint_path Avatar/Wav2Lip/checkpoints/wav2lip_gan.pth --face Avatar/talking.mp4 --audio Avatar/audio.wav"
-        # try:
-        #     os.system(command)
-        #     video_name = "video"+str(uuid.uuid4())+".mp4"
-        #     video_data = {
-        #         "id": index,
-        #         "filename": video_name  # Extract filename if needed
-        #     }
-        #     storage.child(fileName+"videos/"+video_name).put("./Avatar/result_video.mp4")
-        #     database.child(fileName+"videos").child(str(index)).set(video_data)
-        #     print("completed "+ str(index))
-        # except Exception as e:
-        #     print("not completed: "+ e)
+    #     command = "python3 Avatar/Wav2Lip/inference.py --checkpoint_path Avatar/Wav2Lip/checkpoints/wav2lip_gan.pth --face Avatar/talking.mp4 --audio Avatar/audio.wav"
+    #     try:
+    #         os.system(command)
+    #         video_name = "video"+str(uuid.uuid4())+".mp4"
+    #         video_data = {
+    #             "id": index,
+    #             "filename": video_name  # Extract filename if needed
+    #         }
+    #         storage.child(fileName+"videos/"+video_name).put("./Avatar/result_video.mp4")
+    #         database.child(fileName+"videos").child(str(index)).set(video_data)
+    #         print("completed "+ str(index))
+    #     except Exception as e:
+    #         print("not completed: "+ e)
 
-        # index+=1
+    #     index+=1
     return "completed"
 
 @app.route("/")
