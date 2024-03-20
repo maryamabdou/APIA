6e05f1cb48fc1777a1de8b1302c15dfb3cd89402
ingyhany <128065911+ingyhany@users.noreply.github.com>
2024-03-20 16:30:23 +0200

    history

--- client/src/components/Login_Popup.jsx
+++ client/src/components/Login_Popup.jsx
@@ -4,10 +4,13 @@ import Button from './Button';
 import {  useNavigate } from 'react-router-dom';
 import "./LoginPage.css"
 
-
+// const formData2 = {
+//   username: '',
+//   password: ''
+// };
 
 const Login_Popup = () => {
-    
+  ;
     const [formData2, setFormData2] = useState({
       username: '',
       password: ''
@@ -17,28 +20,12 @@ const Login_Popup = () => {
     const [error, setError] = useState('');
     const navigate = useNavigate();
     
-   
-    // if (!username || !password) {
-    //   setError("Please fill in all the fields.");
-    //   return;
-    // }
-  
-    // try {
-    //   const response = await axios.post(endpoint, data);
-  
-    //   if (response.status === 201) {
-    //     navigate("/main");
-    //   } else {
-    //     setError(`Login Failed, Server returned status ${response.status}`);
-    //   }
-    // } catch (error) {
-    //   setError(`Login Failed. ${error.message}`);
-    // }
     
  
  
   const handleSubmit = async (event) => {
     event.preventDefault();
+    console.log(formData2.username)
     if (!formData2.username||!formData2.password ) {
       setError("Please Fill In All The Fields.");
       event.preventDefault();
@@ -51,11 +38,13 @@ const Login_Popup = () => {
           headers: { 'Content-Type': 'application/json' }
       });
       
-      const responseData = await response.json();
-      
+      const responseData = await response.json();      
+  console.log(responseData)
       if (responseData.message === 'success') {
           console.log("Login successful!");
-          navigate('/firstpage');
+          // navigate('/firstpage',formData2.username);
+          // history.push('/firstpage', { username: formData2.username });
+          navigate('/firstpage', { state: { username: formData2.username } });
       } else {
           console.error('Login failed:', responseData);
           alert("Invalid username or password.");
@@ -64,32 +53,7 @@ const Login_Popup = () => {
       console.error('Error:', error);
   }
 };
-//     try {
-//     fetch('/login', {
-//               method: 'POST',
-//               body: JSON.stringify(formData2),
-//               headers: { 'Content-Type': 'application/json' }
-//           })
-//           .then(response => response.json())
-//           .then(responseData => {
-//             console.log(responseData);
-//           //   if (responseData === '200') {
-//           //     console.log("Hello, this is a debug message of login!");
-//           //     navigate('/firstpage');
-//           // } else {
-//           //     console.error('Login failed:', responseData);
-//           //     // Handle login failure, e.g., display error message to user
-//           // }
-//           });
-//       //console.log("formData");
-// //         const response = await axios.post('http://127.0.0.1:5000/signup', formData);
-//       //.log("formData2");
-//       //console.log(response.data);
-//      navigate('/firstpage');
-//     } catch (error) {
-//       console.error('Error:', error);
-//     }
-//   };
+
   
   
   return (
